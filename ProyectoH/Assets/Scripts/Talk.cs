9360bc425a481d7557b36f98550ce29a0b1ae175
argarc03 <argarc03@ucm.es>
2018-08-19 00:21:35 +0200

    Empezado la creacion de NPC y creada fase

---
+++ ProyectoH/Assets/Scripts/Talk.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Talk : MonoBehaviour {
+
+	public GameObject talkIcon;
+	public GameObject Dialogue;
+	public Button Start;
+
+	void Update()
+	{
+		if (talkIcon.activeInHierarchy && Input.GetKeyDown (KeyCode.E)) 
+		{
+			StartCoroutine (StartTalking ());
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D coll)
+	{
+		if (coll.gameObject.tag == "Player")
+			talkIcon.SetActive (true);
+	}
+
+	void OnTriggerExit2D(Collider2D coll)
+	{
+		if (coll.gameObject.tag == "Player") 
+		{
+			talkIcon.SetActive (false);
+			Dialogue.SetActive (false);
+		}
+	}
+
+	IEnumerator StartTalking()
+	{
+		Dialogue.SetActive (true);
+
+		yield return 0.1f;
+
+		Start.onClick.Invoke();
+	}
+}
